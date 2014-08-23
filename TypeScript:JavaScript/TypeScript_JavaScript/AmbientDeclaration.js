8761100892f649fe7237383cba6b043e69caa5e8
Tejas Bhatt <swadhyaystudios@gmail.com>
2014-08-23 01:19:48 -0400

    changes

---
+++ TypeScript:JavaScript/TypeScript_JavaScript/AmbientDeclaration.js
@@ -0,0 +1,5 @@
+/**
+* Created by ShivTej on 8/16/14.
+*/
+/// <reference path="typings/knockout.d.ts"/>
+//# sourceMappingURL=AmbientDeclaration.js.map
---
+++ TypeScript:JavaScript/TypeScript_JavaScript/AmbientDeclaration.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by ShivTej on 8/16/14.
+ */
+
+/// <reference path="typings/knockout.d.ts"/>
+
+declare var ko;
+
+module ambientDeclaration{
+
+}
